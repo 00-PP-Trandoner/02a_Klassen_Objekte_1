1464de3d0c1ae3de402411f8b6214ccd2ac98c81
Normann <trand_oner@gmx.de>
2023-07-26 11:02:56 +0200

    Instanzvariable

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,10 +4,20 @@ public class App {
     
     public static void main(String[] args) {
 
+    /* Instanziierung Klasse --> Objekt */
+    // Cat -- Referenztyp
+    // cat -- Referenzvariable
 
-    }
+     Cat cat1 = new Cat();
+    System.out.println("Blick von aussen:" + cat1);
+    cat1.tellYourAddress();
+    System.out.println("----------------");
 
-   
+    Cat cat2 = new Cat();
+    System.out.println("Blick von aussen:" + cat2);
+    cat2.tellYourAddress();
+}
 
+    
 }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,13 @@
+package com.cc.java;
+
+public class Cat {
+
+//this ----> Instanzvariable 
+
+public void tellYourAddress() {
+    System.out.println("Blick von innenn:" + this);
+    
+} 
+
+
+}
